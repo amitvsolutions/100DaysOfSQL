5e3a7aae00b39573db649c0ace72f20953d57233
amitvsolutions <amitverma.ibmindia@gmail.com>
2023-03-25 18:17:41 +0530

    simple select query with aliasname

---
+++ Phase-1/Day001.sql
@@ -0,0 +1,14 @@
+SELECT 
+   first_name || ' ' || last_name,
+   email
+FROM 
+   customer;
+   
+
+SELECT 5 * 3;
+
+-- Alias_Name
+SELECT 
+   first_name, 
+   last_name AS surname
+FROM customer;
---
+++ readMe.md
@@ -0,0 +1,7 @@
+Phase-1, 2
+https://www.postgresqltutorial.com/
+https://www.postgresqltutorial.com/postgresql-getting-started/what-is-postgresql/
+
+Phase-3, 4
+
+Phase-5
